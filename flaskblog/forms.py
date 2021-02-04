ebded2809a6fbfa1b1cd3fa52d09f9480cbb8f0b
Victor Omoniyi <victoromoniyi@Victors-MacBook-Pro.local>
2021-02-04 18:11:17 +0000

    add profile pictures, update user account, deleted dark mode

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -6,7 +8,6 @@ from flaskblog.models import User
 class RegistrationForm(FlaskForm):
      username = StringField('Username', validators=[DataRequired(),
      Length(min=2, max = 25)])
-
      email = StringField('Email', validators=[DataRequired(), Email()])
      password = PasswordField('Password', validators=[DataRequired()])
      password_confirm = PasswordField('Confirm Password',
@@ -28,3 +29,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+     username = StringField('Username', validators=[DataRequired(),
+     Length(min=2, max = 25)])
+     email = StringField('Email', validators=[DataRequired(), Email()])
+     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png','gif'])])
+     submit = SubmitField('Update your account')
+
+     def validate_username(self,username):
+         if username.data != current_user.username:
+             user = User.query.filter_by(username=username.data).first()
+             if user:
+                 raise ValidationError('Username already exists. Please choose another one.')
+
+     def validate_email(self,email):
+         if email.data != current_user.email:
+             user = User.query.filter_by(email=email.data).first()
+             if user:
+                 raise ValidationError('Email already taken. Please use another one.')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import current_user, login_user, logout_user, login_required
 
@@ -44,6 +47,18 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(7)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pictures', picture_fn)
+    size_output = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(size_output)
+    i.save(picture_path)
+    return picture_fn
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -65,7 +80,21 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+@app.route('/account' , methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect (url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pictures/' + (current_user.image_file))
+    return render_template('account.html', title='Account',image_file=image_file, form=form)
