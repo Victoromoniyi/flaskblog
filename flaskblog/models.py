e8528fe8091e35f4c96d88c12d08390892814c21
Victor Omoniyi <victoromoniyi@Victors-MacBook-Pro.local>
2021-02-14 20:08:50 +0000

    added pagination, more functionality, added reset password but not fully working

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -15,6 +16,18 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
     password = db.Column(db.String(60), nullable=False)
 
+    def get_reset_token(self, expires_sec=1200):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+            return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.email}','{self.username}','{self.image_file}')"
