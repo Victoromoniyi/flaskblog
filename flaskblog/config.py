066a7416aadca32d4ea6106635613314d49b5baf
Victor Omoniyi <victoromoniyi@Victors-MacBook-Pro.local>
2021-03-03 19:30:05 +0000

    added files

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -8,4 +8,3 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    SQLALCHEMY_TRACK_MODIFICATION = False
